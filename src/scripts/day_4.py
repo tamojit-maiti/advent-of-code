f94292de1f7cd2857568a8601bc5de64e8a5c2e6
tamojit-maiti <maiti.tamojit@gmail.com>
2022-12-15 02:33:58 +0530

    day_4 solution

---
+++ src/scripts/day_4.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+
+# Path to data
+DATA_FOLDER_PATH = Path.cwd() / 'src' / 'data'
+FILE_NAME = 'day_4.txt'
+DATA_FILE_PATH = DATA_FOLDER_PATH / FILE_NAME
+
+count_1 = 0
+count_2 = 0
+# Reading the data
+with open(DATA_FILE_PATH) as f:
+    for line in f:
+        left_area = line.split(',')[0]
+        right_area = line.split(',')[1]
+        left_set = set([i for i in range(int(left_area.split('-')[0]), 
+                                            int(left_area.split('-')[1]) + 1)])
+        right_set = set([i for i in range(int(right_area.split('-')[0]), 
+                                            int(right_area.split('-')[1]) + 1)])
+        if (right_set.issubset(left_set)) or (left_set.issubset(right_set)):
+            count_1 += 1
+        if len(right_set.intersection(left_set))>0:
+            count_2 += 1
+
+# Answer to Part I
+print(count_1)
+
+# Answer to Part II
+print(count_2)
